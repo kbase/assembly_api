7e49fca17fcfc354de062f118c76fc33ec5ee76e
JamesJeffryes <jamesgjeffryes@gmail.com>
2019-04-15 09:25:48 -0500

    use ws & tokens properly

--- lib/AssemblyAPI/Utils.py
+++ lib/AssemblyAPI/Utils.py
@@ -1,7 +1,5 @@
 from collections import Counter
 
-from installed_clients.WorkspaceClient import Workspace
-
 
 def _calc_gc_content(sequence):
     bases = Counter(sequence)
@@ -35,8 +33,7 @@ def _convert_contig_set(contig_set):
     }
 
 
-def _get_data_from_ws(workspace_url, ref, included):
-    ws = Workspace(workspace_url)
+def _get_data_from_ws(ws, ref, included):
     info = ws.get_object_info3({'objects': [{'ref': ref}]})['infos'][0]
     if info[2].split("-")[0] == 'KBaseGenomes.ContigSet':
         return _convert_contig_set(ws.get_objects2(
@@ -45,60 +42,58 @@ def _get_data_from_ws(workspace_url, ref, included):
         {'objects': [{'ref': ref, 'included': included}]})['data'][0]['data']
 
 
-def get_assembly_id(workspace_url, ref):
-    ws = Workspace(workspace_url)
+def get_assembly_id(ws, ref):
     return ws.get_object_info3({'objects': [{'ref': ref}]})['infos'][0][1]
 
 
-def get_genome_annotations(workspace_url, ref):
-    ws = Workspace(workspace_url)
+def get_genome_annotations(ws, ref):
     objects = ws.list_referencing_objects([{'ref': ref}])[0]
     return [f'{info[6]}/{info[0]}/{info[4]}' for info in objects
             if info[2].split("-")[0] == 'KBaseGenomes.Genome'
             or info[2].split("-")[0] == 'KBaseGenomeAnnotations.GenomeAnnotation']
 
 
-def get_external_source_info(workspace_url, ref):
+def get_external_source_info(workspace_client, ref):
     included = ['external_source', 'external_source_id', 'external_source_origination_date']
-    return {k: _get_data_from_ws(workspace_url, ref, included)[k] for k in included}
+    return {k: _get_data_from_ws(workspace_client, ref, included)[k] for k in included}
 
 
-def get_stats(workspace_url, ref):
+def get_stats(workspace_client, ref):
     included = ['num_contigs', 'gc_content', 'dna_size']
-    return {k: _get_data_from_ws(workspace_url, ref, included)[k] for k in included}
+    return {k: _get_data_from_ws(workspace_client, ref, included)[k] for k in included}
 
 
-def get_number_contigs(workspace_url, ref):
-    return _get_data_from_ws(workspace_url, ref, ['num_contigs'])['num_contigs']
+def get_number_contigs(workspace_client, ref):
+    return _get_data_from_ws(workspace_client, ref, ['num_contigs'])['num_contigs']
 
 
-def get_gc_content(workspace_url, ref):
-    return _get_data_from_ws(workspace_url, ref, ['gc_content'])['gc_content']
+def get_gc_content(workspace_client, ref):
+    return _get_data_from_ws(workspace_client, ref, ['gc_content'])['gc_content']
 
 
-def get_dna_size(workspace_url, ref):
-    return _get_data_from_ws(workspace_url, ref, ['dna_size'])['dna_size']
+def get_dna_size(workspace_client, ref):
+    return _get_data_from_ws(workspace_client, ref, ['dna_size'])['dna_size']
 
 
-def get_contig_ids(workspace_url, ref):
+def get_contig_ids(workspace_client, ref):
     included = ['contigs/*/contig_id']
-    data = _get_data_from_ws(workspace_url, ref, included)
+    data = _get_data_from_ws(workspace_client, ref, included)
     return [x for x in data.get('contigs', {})]
 
 
-def get_contig_lengths(workspace_url, ref, contig_id_list):
+def get_contig_lengths(workspace_client, ref, contig_id_list):
     included = [f'contigs/{cid}/length' for cid in contig_id_list]
-    data = _get_data_from_ws(workspace_url, ref, included)
+    data = _get_data_from_ws(workspace_client, ref, included)
     return {k: data['contigs'][k].get('length') for k in contig_id_list}
 
 
-def get_contig_gc_content(workspace_url, ref, contig_id_list):
+def get_contig_gc_content(workspace_client, ref, contig_id_list):
     included = [f'contigs/{cid}/gc_content' for cid in contig_id_list]
-    data = _get_data_from_ws(workspace_url, ref, included)
+    data = _get_data_from_ws(workspace_client, ref, included)
     return {k: data['contigs'][k].get('gc_content') for k in contig_id_list}
 
 
-def get_contigs(workspace_url, ref, contig_id_list):
+def get_contigs(workspace_client, ref, contig_id_list):
     included = ['contigs/' + cid for cid in contig_id_list]
-    data = _get_data_from_ws(workspace_url, ref, included)
+    data = _get_data_from_ws(workspace_client, ref, included)
     return {k: data['contigs'][k] for k in contig_id_list}
